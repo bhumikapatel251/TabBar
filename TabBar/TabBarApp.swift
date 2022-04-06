ff6bce01199fc677458bc8b8a7191df98491da18
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-04-06 16:31:43 +0530

    Initial Commit

---
+++ TabBar/TabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarApp.swift
+//  TabBar
+//
+//  Created by Bhumika Patel on 06/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
